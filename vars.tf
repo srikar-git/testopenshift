3a0a2d387c42b024a650cdfb5945480d9a977708
Jagannadha Srikar Uppada <srikarseven@gmail.com>
2019-12-18 17:17:58 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "cluster" {
+  description = "Name or id of cluster"
+  default     = ""
+}
+
+variable "resource_group" {
+  description = "resource group id"
+  default     = "default"
+}
